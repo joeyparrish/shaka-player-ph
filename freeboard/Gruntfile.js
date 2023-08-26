65b8edc1535de1b2a35987ad54e2edb0e4027a70
Joey Parrish <joey.parrish@gmail.com>
2023-08-26 15:30:40 -0700

    Upgrade grunt build system

--- freeboard/Gruntfile.js
+++ freeboard/Gruntfile.js
@@ -19,28 +19,28 @@ module.exports = function(grunt) {
                         'lib/js/thirdparty/underscore.js',
                         'lib/js/thirdparty/jquery.gridster.js',
                         'lib/js/thirdparty/jquery.caret.js',
-						'lib/js/thirdparty/jquery.xdomainrequest.js',
+                        'lib/js/thirdparty/jquery.xdomainrequest.js',
                         'lib/js/thirdparty/codemirror.js',
                     ]
                 ],
                 dest : 'js/freeboard.thirdparty.js'
             },
-			fb : {
-				src : [
-					'lib/js/freeboard/DatasourceModel.js',
-					'lib/js/freeboard/DeveloperConsole.js',
-					'lib/js/freeboard/DialogBox.js',
-					'lib/js/freeboard/FreeboardModel.js',
-					'lib/js/freeboard/FreeboardUI.js',
-					'lib/js/freeboard/JSEditor.js',
-					'lib/js/freeboard/PaneModel.js',
-					'lib/js/freeboard/PluginEditor.js',
-					'lib/js/freeboard/ValueEditor.js',
-					'lib/js/freeboard/WidgetModel.js',
-					'lib/js/freeboard/freeboard.js',
-				],
-				dest : 'js/freeboard.js'
-			},
+            fb : {
+                src : [
+                    'lib/js/freeboard/DatasourceModel.js',
+                    'lib/js/freeboard/DeveloperConsole.js',
+                    'lib/js/freeboard/DialogBox.js',
+                    'lib/js/freeboard/FreeboardModel.js',
+                    'lib/js/freeboard/FreeboardUI.js',
+                    'lib/js/freeboard/JSEditor.js',
+                    'lib/js/freeboard/PaneModel.js',
+                    'lib/js/freeboard/PluginEditor.js',
+                    'lib/js/freeboard/ValueEditor.js',
+                    'lib/js/freeboard/WidgetModel.js',
+                    'lib/js/freeboard/freeboard.js',
+                ],
+                dest : 'js/freeboard.js'
+            },
             plugins : {
                 src : [
                     'plugins/freeboard/*.js'
@@ -88,25 +88,27 @@ module.exports = function(grunt) {
                 }
             }
         },
-        'string-replace': {
+        replace : {
             css: {
-                files: {
-                    'css/': 'css/*.css'
-                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['css/*.css'],
+                    dest: 'css/',
+                }],
                 options: {
-                    replacements: [{
-                        pattern: /..\/..\/..\/img/ig,
+                    patterns: [{
+                        match: /..\/..\/..\/img/ig,
                         replacement: '../img'
-                    }]
-                }
-            }
-        }
+                    }],
+                },
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-string-replace');
-    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:fb', 'concat:thirdparty', 'concat:plugins', 'concat:fb_plugins', 'uglify:fb', 'uglify:plugins', 'uglify:fb_plugins', 'uglify:thirdparty', 'string-replace:css' ]);
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:fb', 'concat:thirdparty', 'concat:plugins', 'concat:fb_plugins', 'uglify:fb', 'uglify:plugins', 'uglify:fb_plugins', 'uglify:thirdparty', 'replace' ]);
 };
